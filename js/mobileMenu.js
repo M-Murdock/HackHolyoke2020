777615af96ebe2263d1bb5ac1994eac4cfe8dc9f
RoseHairTarantula <mavis@somewords.org>
2020-08-04 23:43:43 -0400

    Switch fonts to Silom

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -8,10 +8,12 @@ $(document).ready(function() {
     if($('span').hasClass("closed")) {
       $('span').removeClass("closed");
       $('span').addClass("open");
+      $('header').css("background-color","white");
     }
     else if($('span').hasClass("open")) {
       $('span').removeClass("open");
       $('span').addClass("closed");
+      $('header').css("background-color","rgba(255,255,255,0.3)");
     }
   });
 });
